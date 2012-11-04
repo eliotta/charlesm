70c27604ea7f9a4117b8240a7b4d8bbeea98c84d
eliotta <eliotta2@yahoo.com>
2012-11-04 15:59:37 -0500

    third

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Charlesm::Application.config.secret_token = '7cd526cd11516a5057abd3e0f5ba4d1c0aea75df07b1baf65394c755defd3037a01e3c84e8b63bed089dfe7109e5e8c88638eec6fbfa90bd6d107d7c80b7ae58'
